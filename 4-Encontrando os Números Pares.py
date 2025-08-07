b1be01c2da8d63bc34854a44644b1cf8e3480bf4
luisdoscod <luisotavio70p@gmail.com>
2025-08-07 17:08:08 -0300

    Solução concluída do Encontrando os Números Pares

--- 4-Encontrando os Números Pares.py
+++ 4-Encontrando os Números Pares.py
@@ -7,4 +7,4 @@ def numeros_pares(lista):
 
 numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
 
-print(f'os numeros inteiros da lista são: {numeros_pares(numeros)}')
+print(f'os numeros pares da lista são: {numeros_pares(numeros)}')
