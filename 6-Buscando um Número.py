91eb649a054bd0937ab907c825f2e715f93127b5
luisdoscod <luisotavio70p@gmail.com>
2025-08-07 17:44:25 -0300

    Solução concluída do Buscando um Número

---
+++ 6-Buscando um Número.py
@@ -0,0 +1,7 @@
+def bucar_numero(numero):
+    lista = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+    return numero in lista
+print(bucar_numero(7)) #true
+print(bucar_numero(4)) #false
+print(bucar_numero(56)) #true
+
