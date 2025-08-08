bcfdd6d7e223fba101e66a1c3cf6a4fac774f3a8
luisdoscod <luisotavio70p@gmail.com>
2025-08-08 16:50:46 -0300

    Solução concluída do Juntando Duas Listas

---
+++ 12-Juntando Duas Listas.py
@@ -0,0 +1,7 @@
+def juntar_listas(lista1,lista2):
+    return lista1 + lista2
+
+primeira_lista = [1, 2, 3, 4, 5]
+segunda_lista = [6, 7, 8, 9, 10]
+terceira_lista = juntar_listas(primeira_lista, segunda_lista)
+print(terceira_lista)
