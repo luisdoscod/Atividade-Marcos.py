20c8c5316ca25e711742c3512d03b324e9542595
luisdoscod <luisotavio70p@gmail.com>
2025-08-08 15:26:06 -0300

    Solução concluída do Somando os Valores da Lista

---
+++ 8-Somando os Valores da Lista.py
@@ -0,0 +1,8 @@
+def somar_valores(lista):
+    soma = 0
+    for numero in lista:
+        soma += numero
+    return soma
+
+lista = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+print(somar_valores(lista))
