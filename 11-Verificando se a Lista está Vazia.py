f9f8ca6ad2603f3f621f78b7b02b0a046cf6673b
luisdoscod <luisotavio70p@gmail.com>
2025-08-08 16:05:49 -0300

    Solução concluída do Verificando se a Lista está Vazia

---
+++ 11-Verificando se a Lista está Vazia.py
@@ -0,0 +1,8 @@
+def verificar_lista(lista):
+    if len(lista) == 0:
+        return True
+    else:
+        return False
+
+lista = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+print(verificar_lista(lista)) 
